9bd09c4ace5cfc77a9d717dfb3f5aa1a892e0b17
Alex Sherman <asherman.ca@gmail.com>
2022-11-23 14:26:35 -0800

    note form init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
 import { Routes, Route, Navigate } from 'react-router-dom';
+
+import EditNote from './Components/EditNote';
+import NewNote from './Components/NewNote';
 
 const App = () => {
 	return (
-		<Routes>
-			<Route path='/' element={<h1>home</h1>} />
-			<Route path='/new' element={<h1>new</h1>} />
-			<Route path='/:id'>
-				<Route index element={<h1>show</h1>} />
-				<Route path='edit' element={<h1>edit</h1>} />
-			</Route>
+		<Container className='my-4'>
+			<Routes>
+				<Route path='/' element={<h1>home</h1>} />
+				<Route path='/new' element={<NewNote />} />
+				<Route path='/:id'>
+					<Route index element={<h1>show</h1>} />
+					<Route path='edit' element={<EditNote />} />
+				</Route>
 
-			<Route path='*' element={<Navigate to='/' />} />
-		</Routes>
+				<Route path='*' element={<Navigate to='/' />} />
+			</Routes>
+		</Container>
 	);
 };
 
---
+++ src/Components/EditNote.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import NoteForm from './NoteForm';
+
+const EditNote = () => {
+	return (
+		<>
+			<h1 className='mb-4'>Edit Note</h1>
+			<NoteForm />
+		</>
+	);
+};
+
+export default EditNote;
