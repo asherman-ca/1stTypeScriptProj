10f0eadf80cd31f439bca00703031d93fbaf0499
Alex Sherman <asherman.ca@gmail.com>
2022-11-23 13:03:51 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+const App = () => {
+	return (
+		<Routes>
+			<Route path='/' element={<h1>home</h1>} />
+			<Route path='/new' element={<h1>new</h1>} />
+			<Route path='/:id'>
+				<Route index element={<h1>show</h1>} />
+				<Route path='edit' element={<h1>edit</h1>} />
+			</Route>
+
+			<Route path='*' element={<Navigate to='/' />} />
+		</Routes>
+	);
+};
+
+export default App;
